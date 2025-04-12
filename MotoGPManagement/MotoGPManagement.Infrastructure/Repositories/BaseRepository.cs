f9ac6bd757c247f59bdddc1ad5b7b6201021d22f
ayer32 <48725957+ayer32@users.noreply.github.com>
2025-04-12 14:44:49 +0545

    Add MotoGPManagement/MotoGPManagement.Infrastructure/Repositories/BaseRepository.cs 1744448388712

---
+++ MotoGPManagement/MotoGPManagement.Infrastructure/Repositories/BaseRepository.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MotoGPManagement.Core.Interfaces;
+using MotoGPManagement.Infrastructure.Data;
+
+namespace MotoGPManagement.Infrastructure.Repositories
+{
+    public class BaseRepository<T> : IRepository<T> where T : class
+    {
+        protected readonly ApplicationDbContext _context;
+        protected readonly DbSet<T> _dbSet;
+
+        public BaseRepository(ApplicationDbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+        public virtual async Task<T> GetByIdAsync(Guid id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public virtual async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public virtual async Task<T> AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public virtual async Task UpdateAsync(T entity)
+        {
+            _dbSet.Update(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public virtual async Task DeleteAsync(Guid id)
+        {
+            var entity = await GetByIdAsync(id);
+            if (entity != null)
+            {
+                _dbSet.Remove(entity);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
